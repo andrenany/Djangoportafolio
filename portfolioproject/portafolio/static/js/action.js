1c4a3f2a1f5b140d57e7966f90c2c501f246ef4d
andrenany <fa.araujo@duocuc.cl>
2024-07-03 23:24:38 -0400

    secod commit

---
+++ portfolioproject/portafolio/static/js/action.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const multipleText = document.querySelector('.multiple-text');
+  const professions = ['Administración ', 'Informática '];
+  let index = 0, charIndex = 0, isDeleting = false;
+
+  function type() {
+      const currentProfession = professions[index];
+      const displayText = isDeleting 
+          ? currentProfession.substring(0, charIndex--) 
+          : currentProfession.substring(0, charIndex++);
+      
+      multipleText.textContent = displayText;
+
+      if (!isDeleting && charIndex === currentProfession.length) {
+          isDeleting = true;
+          setTimeout(type, 2000);
+      } else if (isDeleting && charIndex === 0) {
+          isDeleting = false;
+          index = (index + 1) % professions.length;
+          setTimeout(type, 500);
+      } else {
+          setTimeout(type, isDeleting ? 50 : 100);
+      }
+  }
+
+  type();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  let form = document.getElementById("myForm");
+
+  form.addEventListener("submit", function(event) {
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("correo").value;
+    let mensaje = document.getElementById("mensaje").value;
+
+    // Valido los campos
+    if (nombre.trim() === '') {
+      alert('Por favor, ingrese su nombre completo.');
+      event.preventDefault();
+      return;
+    }
+
+    if (correo.trim() === '') {
+      alert('Por favor, ingrese su correo electrónico.');
+      event.preventDefault();
+      return;
+    }
+
+    if (mensaje.trim() === '') {
+      alert('Por favor, ingrese un mensaje.');
+      event.preventDefault();
+      return;
+    }
+
+    // Envío el formulario
+    form.submit();
+  });
+});
+
+
